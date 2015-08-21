c09760cd3af550f9ee857f813e831b5aa5253aab
N K <konancys@gmail.com>
2015-08-20 22:22:42 -0400

    Done

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+5.times do 
+	Artist.create({
+		name: Faker::Name.name
+		}) 	
+end
+
+10.times do 
+	Genre.create({
+		name: Faker::Address.country
+		})
+end
+
+10.times do
+	genres = Genre.all.sort
+	artists = Artist.all.sort
+	Song.create({
+		name: Faker::Book.title,
+		artist_id: artists[rand(1..5)].id,
+		genre_id: genres[rand(1..10)].id
+	})
+end
